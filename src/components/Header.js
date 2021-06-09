d51ff8f5538d04cefb55a89e30db4278d551ff92
Pranay Gupta <pragupta36@publicisgroupe.net>
2021-06-09 23:29:57 +0530

    converting header into a stateless component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
 import React, {Component} from 'react';
 
-class Header extends Component {
-    render(){
+const Header = (props) => {
         return(
             <nav className="navbar navbar-default">
                 <div className="container">
                     <div className="navbar-header">
                         <ul className="nav navbar-nav">
-                            <li><a href="#">Home</a></li>
+                            <li><a href={props.homeLink}>Home</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
         );
-    }
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class App extends Component{
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                       <Header />
+                       <Header homeLink={"Home"}/>
                     </div>
                 </div>
                 <div className="row">
