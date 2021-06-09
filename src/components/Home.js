1be942a4ec2ee43521ad777e1f9c24a2a0587a87
Pranay Gupta <pragupta36@publicisgroupe.net>
2021-06-09 22:10:03 +0530

    setted up the home and header

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <p>In a new Component!</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import Home from './components/Home';
 
 class App extends Component{
     render(){
@@ -7,7 +9,12 @@ class App extends Component{
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>Hello</h1>
+                       <Header />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                       <Home />
                     </div>
                 </div>
             </div>
